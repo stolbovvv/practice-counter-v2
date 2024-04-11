6405063a0802ef229a11468e8b45306629a114ab
Valerii Stolbov <stolbov.v@outlook.com>
2024-04-11 17:36:29 +0500

    Update URL

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/practice-react-redux-counter/',
+  base: '/practice-counter-v2/',
   plugins: [react()],
 });
